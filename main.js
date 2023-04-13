56236885b47b9b28f1add506d423864519c08620
Elissa Martial <35646763+elissajmar@users.noreply.github.com>
2023-04-13 10:30:16 -0700

    working on home page
    I also created universal js and css pages, i predefined the color and font styles so please use those (in main.css) so that we can easily change the colors/font styles in the future if we need to!

---
+++ main.js
@@ -0,0 +1,5 @@
+// Serves as the overarching JS that should apply to the entirety of the website
+
+// For example, we can define the header navigation here and 
+// just reference this file in every other html file.
+
