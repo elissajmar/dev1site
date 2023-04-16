d8bb95a90153bc7b9c5389be0936db30577dc2ad
YejiUSC <fseo@usc.edu>
2023-04-15 17:27:51 -0700

    new footer + edited products

--- products/products.js
+++ products/products.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
     function changeSlide(n) {
         for (var i = 0; i < imgs.length; i++) { // reset
         imgs[i].style.opacity = 0;
-        dots[i].className = dots[i].className.replace(' active', '');
+        dots[i].className = 'dots';
         }
     
         currentImg = n;
