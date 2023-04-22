2b381963a8180626b5042cdb0ed84234701494d2
Elissa Martial <35646763+elissajmar@users.noreply.github.com>
2023-04-21 22:07:57 -0700

    adding cart functionality with localStorage

--- products/products.js
+++ products/products.js
@@ -1,11 +1,10 @@
-
 $(document).ready(function(){
-
+    //PRODUCT IMAGE GALLERIES
     var imgs = document.querySelectorAll('.slider img');
     var dots = document.querySelectorAll('.dot');
     var currentImg = 0; // index of the first image 
     const interval = 3000; // duration(speed) of the slide
-
+    
     function changeSlide(n) {
         for (var i = 0; i < imgs.length; i++) { // reset
         imgs[i].style.opacity = 0;
@@ -17,4 +16,89 @@ $(document).ready(function(){
         imgs[currentImg].style.opacity = 1;
         dots[currentImg].className += ' active';
     }
+
+    /* CART FUNCTIONALITY */
+    //localStorage.clear()
+
+    /* DEFINING PRODUCTS */
+    let topBag = { //defines the template for a product with name, price, and quantity
+        "name": "The Top Bag",
+        "price": 49.99,
+    }
+
+    let bottomBag = {
+        "name": "The Bottom Bag",
+        "price": 49.99,
+    }
+
+    //on add to cart button click, call add to cart for that specific product
+    $(".addtocartTop").click(function(){
+        quantity = $("#quantityTop").val()
+        addToCart(topBag, quantity)
+        
+        console.log(cart) //to check
+    })
+
+    $(".addtocartBottom").click(function(){
+        quantity = $("#quantityBottom").val()
+        addToCart(bottomBag, quantity)
+
+        console.log(cart)//to check
+    })
+
+
+    /* FUNCTIONS */
+    function setCart (cart){ // stores cart in localStorage 
+        localStorage.setItem("cart", JSON.stringify(cart))
+    }
+
+    function getCart(){ // gets cart from localStorage and parses it
+        if (localStorage.getItem("cart")){
+            cart = JSON.parse(localStorage.getItem("cart"))
+        }
+        else {
+            cart = []
+        }
+        return cart
+    }
+
+    function addToCart(product, quantity){ //where productName is an object 
+        cart = getCart() //get the cart
+        quantity = parseFloat(quantity) //make sure quantity is a number
+
+        if (cart.length == 0 ){ //check if cart length is 0, then autoadd 
+            console.log("zero")
+            product.quantity = quantity;
+            cart.push(product)
+        }
+
+        else {
+            added = false;
+
+            //check if cart already has product
+            for (i=0; i<cart.length; i++){   //for the length of the cart
+                if (cart[i].name == product.name){ //if theres a product in the cart already that matches
+                    console.log("yes")
+                    cart[i].quantity = parseFloat(cart[i].quantity) + quantity; //increment the quantity
+                    added = true;
+                }
+            }
+
+            if (added == false){ //if no match was found
+                product.quantity = quantity; //add a new product to the cart
+                cart.push(product)
+            }
+        }
+        
+        setCart(cart) //stores cart in storage
+    }
+
+    function clearCart(){ //restarts the cart
+        cart = getCart()
+        cart = []
+        setCart(cart)
+    }
+
+    
+
 })
